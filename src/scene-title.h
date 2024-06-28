b0ea37d21258e5afd3996f43ddf431d2b437e0e8
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2024-06-28 15:22:44 -0500

    Modularization of scene management code
    I've decided to change the way that "scenes" are handled, having each one exist in its own module. I'm still deciding whether I want each scene to have its own "game loop" or to execute frame-by-frame operations in the main method in main.c... leaning on the former rather than the latter, but we'll see.

---
+++ src/scene-title.h
@@ -0,0 +1,10 @@
+#include <gb/gb.h>
+#include <stdint.h>
+#include "../res/bkg_title.h"
+#include "../res/spr_star.h"
+
+/* Initialize the background and sprites of the scene */
+void InitGraphics_Title(void);
+
+/* Operates the actions that sprites perform on a frame-by-frame basis */
+void SpriteActivity_Title(void);
